03bea9eef6560c7b5f1a8ff6f5567888b3387201
Nicolás Vescio <nicolas.vescio@wolox.com.ar>
2019-02-19 12:36:09 -0300

    Routing

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from './views/Register'
+import Login from './views/Login'
 
 Vue.use(Router)
 
@@ -8,16 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/sign-up',
       name: 'register',
       component: Register
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
